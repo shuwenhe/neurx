2b9ad61b8d641f6d117879f1f232f81e85254f1f
shuwen <1201220707@pku.edu.cn>
2026-03-03 10:39:11 +0800

    feat:bce_loss

--- python/tensor/nn/__init__.py
+++ python/tensor/nn/__init__.py
@@ -1,5 +1,17 @@
 from tensor.nn.modules import *
 from tensor.nn import functional
+from tensor.nn.functional import (
+    # Activation functions
+    relu, sigmoid, tanh, elu, selu, prelu, rrelu, hardtanh, hardswish, mish,
+    silu, gelu,
+    # Normalization
+    softmax, log_softmax, layer_norm, rms_norm,
+    # Operations
+    linear, dropout, embedding,
+    # Loss functions
+    mse_loss, cross_entropy, nll_loss,
+    bce_loss, bce_with_logits_loss, l1_loss, smooth_l1_loss, kl_div_loss,
+)
 
 __all__ = [
     "Parameter",
@@ -21,4 +33,14 @@ __all__ = [
     "MoE",
     "TransformerBlock",
     "functional",
+    # Activation functions
+    "relu", "sigmoid", "tanh", "elu", "selu", "prelu", "rrelu", 
+    "hardtanh", "hardswish", "mish", "silu", "gelu",
+    # Normalization
+    "softmax", "log_softmax", "layer_norm", "rms_norm",
+    # Operations
+    "linear", "dropout", "embedding",
+    # Loss functions
+    "mse_loss", "cross_entropy", "nll_loss",
+    "bce_loss", "bce_with_logits_loss", "l1_loss", "smooth_l1_loss", "kl_div_loss",
 ]
