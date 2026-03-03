c3a0928e3a42d014835fffe2bad9a7176e65aa33
shuwen <1201220707@pku.edu.cn>
2026-03-03 10:48:31 +0800

    fix:tensor

--- python/tensor/nn/__init__.py
+++ python/tensor/nn/__init__.py
@@ -2,12 +2,12 @@ from tensor.nn.modules import *
 from tensor.nn import functional
 from tensor.nn.functional import (
     # Activation functions
-    relu, sigmoid, tanh, elu, selu, prelu, rrelu, hardtanh, hardswish, mish,
+    relu, leaky_relu, sigmoid, tanh, elu, selu, prelu, rrelu, hardtanh, hardswish, mish,
     silu, gelu,
     # Normalization
     softmax, log_softmax, layer_norm, rms_norm,
     # Operations
-    linear, dropout, embedding,
+    linear, conv2d, max_pool2d, avg_pool2d, adaptive_avg_pool2d, dropout, embedding,
     # Loss functions
     mse_loss, cross_entropy, nll_loss,
     bce_loss, bce_with_logits_loss, l1_loss, smooth_l1_loss, kl_div_loss,
@@ -34,12 +34,12 @@ __all__ = [
     "TransformerBlock",
     "functional",
     # Activation functions
-    "relu", "sigmoid", "tanh", "elu", "selu", "prelu", "rrelu", 
+    "relu", "leaky_relu", "sigmoid", "tanh", "elu", "selu", "prelu", "rrelu", 
     "hardtanh", "hardswish", "mish", "silu", "gelu",
     # Normalization
     "softmax", "log_softmax", "layer_norm", "rms_norm",
     # Operations
-    "linear", "dropout", "embedding",
+    "linear", "conv2d", "max_pool2d", "avg_pool2d", "adaptive_avg_pool2d", "dropout", "embedding",
     # Loss functions
     "mse_loss", "cross_entropy", "nll_loss",
     "bce_loss", "bce_with_logits_loss", "l1_loss", "smooth_l1_loss", "kl_div_loss",
