16b919fffba74d1b36cb4bdef09ec8a3de64aad4
shuwen <1201220707@pku.edu.cn>
2026-03-03 10:05:34 +0800

    feat:scheduler

--- python/tensor/optim/__init__.py
+++ python/tensor/optim/__init__.py
@@ -1,9 +1,26 @@
 from tensor.optim.optimizer import Optimizer
 from tensor.optim.optim import *
+from tensor.optim.scheduler import (
+    StepLR,
+    ExponentialLR,
+    CosineAnnealingLR,
+    CosineAnnealingWarmRestarts,
+    ReduceLROnPlateau,
+    LinearLR,
+    LambdaLR,
+)
 
 __all__ = [
     "Optimizer",
     "SGD",
     "AdamW",
     "clip_grad_norm",
+    # Schedulers
+    "StepLR",
+    "ExponentialLR",
+    "CosineAnnealingLR",
+    "CosineAnnealingWarmRestarts",
+    "ReduceLROnPlateau",
+    "LinearLR",
+    "LambdaLR",
 ]
