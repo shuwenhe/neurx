92fe28a07fa34012babfddfe9b79db2a5cfabbcd
shuwen <1201220707@pku.edu.cn>
2026-04-01 00:52:51 +0800

    auto: sync 2026-03-31 16:52:51 UTC

--- python/neurx/distributed/__init__.py
+++ python/neurx/distributed/__init__.py
@@ -1,9 +1,27 @@
 from neurx.distributed.launcher import DistributedConfig, detect_distributed_config, is_distributed, validate_distributed_config
+from neurx.nn.distributed import (
+    all_reduce,
+    barrier,
+    broadcast,
+    destroy_process_group,
+    get_rank,
+    get_world_size,
+    init_process_group,
+    is_initialized,
+)
 
 __all__ = [
     "DistributedConfig",
     "detect_distributed_config",
     "validate_distributed_config",
     "is_distributed",
+    "init_process_group",
+    "destroy_process_group",
+    "is_initialized",
+    "all_reduce",
+    "broadcast",
+    "barrier",
+    "get_rank",
+    "get_world_size",
 ]
 
