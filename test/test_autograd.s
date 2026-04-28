679fb39cd1e7111e6b71a30917e05102d1afbc22
shuwen <1201220707@pku.edu.cn>
2026-04-28 08:32:39 +0000

    style: normalize s naming and type declaration format

--- test/test_autograd.s
+++ test/test_autograd.s
@@ -1,13 +1,12 @@
 package neurx.test_autograd
 
-use neurx.tensor.{Tensor, new, add}
+use neurx.tensor.{tensor, new, add}
 use neurx.autograd.backward
 
 func main() int32 {
     let a = new([1.0, 2.0], [2], true)
     let b = new([3.0, 4.0], [2], true)
     let c = add(a, b)
-    // 假设 c 作为 loss，调用反向传播
     backward(c)
     println("a.grad = ", a.grad)
     println("b.grad = ", b.grad)
--- test/test_schedule.s
+++ test/test_schedule.s
@@ -1,10 +1,10 @@
 package neurx.test_schedule
 
-use neurx.schedule.{Op, run}
+use neurx.schedule.{op, run}
 
 func main() int32 {
-    let op = Op { name: "add" }
-    run(op)
+    let op_item = op { name: "add" }
+    run(op_item)
     println("调度器执行完成")
     0
 }
