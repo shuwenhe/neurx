78ca1ea071075f1b83f469d0e49b86a6b1e9e45d
shuwen <1201220707@pku.edu.cn>
2026-04-28 08:57:07 +0000

    feat: practicalize autograd and add adam/rmsprop with minimal dataloader

--- s/autograd.s
+++ s/autograd.s
@@ -5,5 +5,108 @@ use neurx.tensor.tensor
 struct grad_fn {
 }
 
+struct grad_record {
+	int id
+	[]int shape
+	bool requires_grad
+	[]f32 grad
+}
+
+struct autograd_state {
+	[]grad_record records
+}
+
+func new_state() autograd_state {
+	autograd_state {
+		records: [],
+	}
+}
+
+func zeros_like([]f32 data) []f32 {
+	let n = len(data)
+	let mut out = []f32{cap: n}
+	for i in 0..n {
+		out.push(0.0)
+	}
+	out
+}
+
+func ones_like([]f32 data) []f32 {
+	let n = len(data)
+	let mut out = []f32{cap: n}
+	for i in 0..n {
+		out.push(1.0)
+	}
+	out
+}
+
+func register_tensor(autograd_state state, int id, tensor value) autograd_state {
+	let mut records = state.records
+	let grad_data = zeros_like(value.data)
+	records.push(
+		grad_record {
+			id: id,
+			shape: value.shape,
+			requires_grad: value.requires_grad,
+			grad: grad_data,
+		}
+	)
+
+	autograd_state {
+		records: records,
+	}
+}
+
+func set_grad(autograd_state state, int id, []f32 grad) autograd_state {
+	let mut records = state.records
+	let n = len(records)
+	for i in 0..n {
+		if records[i].id == id {
+			records[i].grad = grad
+		}
+	}
+
+	autograd_state {
+		records: records,
+	}
+}
+
+func accumulate_grad(autograd_state state, int id, []f32 grad) autograd_state {
+	let mut records = state.records
+	let n = len(records)
+	for i in 0..n {
+		if records[i].id == id {
+			let m = len(records[i].grad)
+			if m == len(grad) {
+				for j in 0..m {
+					records[i].grad[j] = records[i].grad[j] + grad[j]
+				}
+			}
+		}
+	}
+
+	autograd_state {
+		records: records,
+	}
+}
+
+func grad_of(autograd_state state, int id) []f32 {
+	let n = len(state.records)
+	for i in 0..n {
+		if state.records[i].id == id {
+			return state.records[i].grad
+		}
+	}
+	[]
+}
+
+func backward_seed(autograd_state state, int loss_id, tensor loss_tensor) autograd_state {
+	if !loss_tensor.requires_grad {
+		return state
+	}
+
+	set_grad(state, loss_id, ones_like(loss_tensor.data))
+}
+
 func backward(tensor t) () {
 }
--- s/trainer.s
+++ s/trainer.s
@@ -1,7 +1,7 @@
 package neurx.trainer
 
 use neurx.multimodal.multimodal_batch
-use neurx.optim_mvp.{sgd_optimizer, new_sgd, step_tensor}
+use neurx.optim_mvp.{sgd_optimizer, adam_optimizer, rmsprop_optimizer, new_sgd, new_adam, new_rmsprop, step_tensor, adam_step, rmsprop_step}
 use neurx.tensor.tensor
 
 struct trainer_config {
@@ -15,6 +15,8 @@ struct trainer_state {
     int step
     f32 last_loss
     sgd_optimizer optimizer
+    adam_optimizer adam
+    rmsprop_optimizer rmsprop
 }
 
 struct trainer_step_output {
@@ -36,6 +38,8 @@ func new_state() trainer_state {
         step: 0,
         last_loss: 0.0,
         optimizer: new_sgd(0.001),
+        adam: new_adam(0.001, 0.9, 0.999, 1e-8),
+        rmsprop: new_rmsprop(0.001, 0.99, 1e-8),
     }
 }
 
@@ -44,6 +48,8 @@ func init_state(trainer_config config) trainer_state {
         step: 0,
         last_loss: 0.0,
         optimizer: new_sgd(config.learning_rate),
+        adam: new_adam(config.learning_rate, 0.9, 0.999, 1e-8),
+        rmsprop: new_rmsprop(config.learning_rate, 0.99, 1e-8),
     }
 }
 
@@ -59,6 +65,8 @@ func train_step(trainer_state state, multimodal_batch batch) trainer_state {
         step: next_step,
         last_loss: loss,
         optimizer: state.optimizer,
+        adam: state.adam,
+        rmsprop: state.rmsprop,
     }
 }
 
@@ -67,5 +75,37 @@ func apply_sgd(trainer_state state, tensor params, tensor grads) trainer_step_ou
     trainer_step_output {
         state: state,
         params: updated_params,
+    }
+}
+
+func apply_adam(trainer_state state, tensor params, tensor grads) trainer_step_output {
+    let step_output = adam_step(state.adam, params, grads)
+    let next_state = trainer_state {
+        step: state.step,
+        last_loss: state.last_loss,
+        optimizer: state.optimizer,
+        adam: step_output.optimizer,
+        rmsprop: state.rmsprop,
+    }
+
+    trainer_step_output {
+        state: next_state,
+        params: step_output.params,
+    }
+}
+
+func apply_rmsprop(trainer_state state, tensor params, tensor grads) trainer_step_output {
+    let step_output = rmsprop_step(state.rmsprop, params, grads)
+    let next_state = trainer_state {
+        step: state.step,
+        last_loss: state.last_loss,
+        optimizer: state.optimizer,
+        adam: state.adam,
+        rmsprop: step_output.optimizer,
+    }
+
+    trainer_step_output {
+        state: next_state,
+        params: step_output.params,
     }
 }
