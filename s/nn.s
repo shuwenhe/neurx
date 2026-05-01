f4b7dca1329ff3921bec864bcc97045d8cc2761f
Shuwen He <1201220707@pku.edu.cn>
2026-05-01 12:19:22 +0800

    Expand S nn runtime surface

--- s/nn.s
+++ s/nn.s
@@ -50,3 +50,31 @@ func linear_forward(linear layer, tensor input) tensor {
 
     neurx.tensor.new(out, [batch, out_features], input.requires_grad)
 }
+
+func layer_norm(tensor input, tensor weight, tensor bias, int normalized_dims, float eps) tensor {
+    layer_norm(input, weight, bias, normalized_dims, eps)
+}
+
+func rms_norm(tensor input, tensor weight, tensor bias, int normalized_dims, float eps) tensor {
+    rms_norm(input, weight, bias, normalized_dims, eps)
+}
+
+func mlp_block(tensor input, tensor fc1_weight, tensor fc1_bias, tensor fc2_weight, tensor fc2_bias) tensor {
+    mlp_block(input, fc1_weight, fc1_bias, fc2_weight, fc2_bias)
+}
+
+func transformer_block_forward(tensor input, tensor ln1_weight, tensor ln1_bias, tensor qkv_weight, tensor qkv_bias, tensor out_weight, tensor out_bias, tensor ln2_weight, tensor ln2_bias, tensor fc1_weight, tensor fc1_bias, tensor fc2_weight, tensor fc2_bias, float eps, int n_heads) tensor {
+    transformer_block_forward(input, ln1_weight, ln1_bias, qkv_weight, qkv_bias, out_weight, out_bias, ln2_weight, ln2_bias, fc1_weight, fc1_bias, fc2_weight, fc2_bias, eps, n_heads)
+}
+
+func qkv_projection(tensor input, tensor weight, tensor bias, int n_heads) tensor {
+    qkv_projection(input, weight, bias, n_heads)
+}
+
+func rope_apply(tensor input, tensor cos, tensor sin) tensor {
+    rope_apply(input, cos, sin)
+}
+
+func embedding_lookup(tensor weight, tensor input_ids, int padding_idx) tensor {
+    embedding_lookup(weight, input_ids, padding_idx)
+}
