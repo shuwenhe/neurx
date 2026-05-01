affdb68fba3c0e7325a3955c345eb62fef3642b5
Shuwen He <1201220707@pku.edu.cn>
2026-05-02 07:57:43 +0800

    Add S checkpoint runtime coverage

--- s/checkpoint.s
+++ s/checkpoint.s
@@ -8,15 +8,26 @@ struct checkpoint {
     tensor[] params
 }
 
+func new_checkpoint(int step, float loss, tensor[] params) checkpoint {
+    checkpoint {
+        step: step,
+        loss: loss,
+        params: params,
+    }
+}
+
+func checkpoint_state_dict(checkpoint state) checkpoint {
+    state
+}
+
+func checkpoint_load_state_dict(checkpoint state, checkpoint other) checkpoint {
+    other
+}
+
 func save_checkpoint(string path, int step, float loss, tensor[] params) () {
-    
+    new_checkpoint(step, loss, params)
 }
 
 func load_checkpoint(string path) checkpoint {
-    
-    checkpoint {
-        step: 0,
-        loss: 0.0,
-        params: [],
-    }
+    new_checkpoint(0, 0.0, [])
 }
