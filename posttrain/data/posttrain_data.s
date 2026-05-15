5a7cb992d0e29b00f952c7aae72cd1f61380285e
shuwen <1201220707@pku.edu.cn>
2026-05-15 01:40:32 +0000

    Add pretrain/posttrain modules and integrate posttrain pipeline

---
+++ posttrain/data/posttrain_data.s
@@ -0,0 +1,62 @@
+package neurx.posttrain.data
+
+struct posttrain_data_state {
+    string dataset_name
+    string sample_mode
+    int pair_cursor
+    int sample_cursor
+    int epoch
+    bool exhausted
+}
+
+func new_posttrain_data_state(string dataset_name, string sample_mode) posttrain_data_state {
+    posttrain_data_state {
+        dataset_name: dataset_name,
+        sample_mode: sample_mode,
+        pair_cursor: 0,
+        sample_cursor: 0,
+        epoch: 0,
+        exhausted: false,
+    }
+}
+
+func advance_pairs(posttrain_data_state state, int pair_count) posttrain_data_state {
+    posttrain_data_state {
+        dataset_name: state.dataset_name,
+        sample_mode: state.sample_mode,
+        pair_cursor: state.pair_cursor + pair_count,
+        sample_cursor: state.sample_cursor,
+        epoch: state.epoch,
+        exhausted: state.exhausted,
+    }
+}
+
+func advance_samples(posttrain_data_state state, int sample_count) posttrain_data_state {
+    posttrain_data_state {
+        dataset_name: state.dataset_name,
+        sample_mode: state.sample_mode,
+        pair_cursor: state.pair_cursor,
+        sample_cursor: state.sample_cursor + sample_count,
+        epoch: state.epoch,
+        exhausted: state.exhausted,
+    }
+}
+
+func next_epoch(posttrain_data_state state) posttrain_data_state {
+    posttrain_data_state {
+        dataset_name: state.dataset_name,
+        sample_mode: state.sample_mode,
+        pair_cursor: 0,
+        sample_cursor: 0,
+        epoch: state.epoch + 1,
+        exhausted: false,
+    }
+}
+
+func posttrain_data_state_dict(posttrain_data_state state) posttrain_data_state {
+    state
+}
+
+func posttrain_data_load_state_dict(posttrain_data_state state, posttrain_data_state other) posttrain_data_state {
+    other
+}
---
+++ pretrain/data/pretrain_data.s
@@ -0,0 +1,79 @@
+package neurx.pretrain.data
+
+struct pretrain_data_state {
+    string dataset_name
+    int shard_id
+    int num_shards
+    int token_cursor
+    int sample_cursor
+    int epoch
+    bool exhausted
+}
+
+func new_pretrain_data_state(string dataset_name, int shard_id, int num_shards) pretrain_data_state {
+    pretrain_data_state {
+        dataset_name: dataset_name,
+        shard_id: shard_id,
+        num_shards: num_shards,
+        token_cursor: 0,
+        sample_cursor: 0,
+        epoch: 0,
+        exhausted: false,
+    }
+}
+
+func advance_tokens(pretrain_data_state state, int token_count) pretrain_data_state {
+    pretrain_data_state {
+        dataset_name: state.dataset_name,
+        shard_id: state.shard_id,
+        num_shards: state.num_shards,
+        token_cursor: state.token_cursor + token_count,
+        sample_cursor: state.sample_cursor,
+        epoch: state.epoch,
+        exhausted: state.exhausted,
+    }
+}
+
+func advance_samples(pretrain_data_state state, int sample_count) pretrain_data_state {
+    pretrain_data_state {
+        dataset_name: state.dataset_name,
+        shard_id: state.shard_id,
+        num_shards: state.num_shards,
+        token_cursor: state.token_cursor,
+        sample_cursor: state.sample_cursor + sample_count,
+        epoch: state.epoch,
+        exhausted: state.exhausted,
+    }
+}
+
+func mark_exhausted(pretrain_data_state state, bool exhausted) pretrain_data_state {
+    pretrain_data_state {
+        dataset_name: state.dataset_name,
+        shard_id: state.shard_id,
+        num_shards: state.num_shards,
+        token_cursor: state.token_cursor,
+        sample_cursor: state.sample_cursor,
+        epoch: state.epoch,
+        exhausted: exhausted,
+    }
+}
+
+func next_epoch(pretrain_data_state state) pretrain_data_state {
+    pretrain_data_state {
+        dataset_name: state.dataset_name,
+        shard_id: state.shard_id,
+        num_shards: state.num_shards,
+        token_cursor: 0,
+        sample_cursor: 0,
+        epoch: state.epoch + 1,
+        exhausted: false,
+    }
+}
+
+func pretrain_data_state_dict(pretrain_data_state state) pretrain_data_state {
+    state
+}
+
+func pretrain_data_load_state_dict(pretrain_data_state state, pretrain_data_state other) pretrain_data_state {
+    other
+}
