5ac923cd08f0ce57b47fc8f2a9860bca834b6bd0
shuwen <1201220707@pku.edu.cn>
2026-07-09 17:36:50 +0800

    chore: auto-commit changes at 2026-07-09 17:36:50

--- shard/build.s
+++ shard/build.s
@@ -79,6 +79,7 @@ func main() int {
     }
 
     if !compile_one(compiler, script_dir, build_dir, "shard.s") { return 1 }
+    if !compile_one(compiler, script_dir, build_dir, "shard_wikipedia_simple.s") { return 1 }
     if !compile_one(compiler, script_dir, build_dir, "shard_wikipedia.s") { return 1 }
     if !compile_one(compiler, script_dir, build_dir, "load_shards.s") { return 1 }
     if !compile_one(compiler, script_dir, build_dir, "generate_shards.s") { return 1 }
