d65d9f491fd2ecff5660fc6a503e83c965b2ed37
shuwen <1201220707@pku.edu.cn>
2026-07-09 17:09:45 +0800

    chore: auto-commit changes at 2026-07-09 17:09:45

---
+++ test_struct.s
@@ -0,0 +1,13 @@
+// Simple struct test
+struct TestStruct {
+    string name
+    int value
+}
+
+func main() int {
+    TestStruct test
+    test.name = "hello"
+    test.value = 42
+    println("Test completed")
+    0
+}
