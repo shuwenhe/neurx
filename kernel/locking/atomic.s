e8cd30b660ab9a2bf1d3252ce34c0c073e1bd0fe
shuwenhe <1201220707@pku.edu.cn>
2026-08-25 17:58:29 +0800

    fix:lock

--- kernel/locking/atomic.s
+++ kernel/locking/atomic.s
@@ -1,81 +1,14 @@
 package neurx.kernel.locking
 
 struct atomic_int {
-    int value              // Atomic integer value
-    int increment_count    // Total increments
-    int decrement_count    // Total decrements
-    int compare_swap_count // Total CAS operations
+    int value
+    int increment_count
+    int decrement_count
+    int compare_swap_count
 }
 
 func create_atomic(int initial_value) atomic_int {
-    atomic_int {
-        value: initial_value,
-        increment_count: 0,
-        decrement_count: 0,
-        compare_swap_count: 0
-    }
-}
-
-func atomic_increment(mut a: &atomic_int) atomic_int {
-    atom_local := a.*
-    atom_local.value = atom_local.value + 1
-    atom_local.increment_count = atom_local.increment_count + 1
-    
-    a.* = atom_local
-    atom_local
-}
-
-func atomic_decrement(mut a: &atomic_int) atomic_int {
-    atom_local := a.*
-    atom_local.value = atom_local.value - 1
-    atom_local.decrement_count = atom_local.decrement_count + 1
-    
-    a.* = atom_local
-    atom_local
-}
-
-func atomic_add(mut a: &atomic_int, int delta) atomic_int {
-    atom_local := a.*
-    atom_local.value = atom_local.value + delta
-    atom_local.increment_count = atom_local.increment_count + 1
-    
-    a.* = atom_local
-    atom_local
-}
-
-func atomic_sub(mut a: &atomic_int, int delta) atomic_int {
-    atom_local := a.*
-    atom_local.value = atom_local.value - delta
-    atom_local.decrement_count = atom_local.decrement_count + 1
-    
-    a.* = atom_local
-    atom_local
-}
-
-func atomic_compare_and_swap(mut a: &atomic_int, int expected, int new_value) (atomic_int, bool) {
-    atom_local := a.*
-    success := false
-    
-    if atom_local.value == expected {
-        atom_local.value = new_value
-        success = true
-        atom_local.compare_swap_count = atom_local.compare_swap_count + 1
-    }
-    
-    a.* = atom_local
-    (atom_local, success)
-}
-
-func atomic_get(atomic_int a) int {
-    a.value
-}
-
-func atomic_set(mut a: &atomic_int, int new_value) atomic_int {
-    atom_local := a.*
-    atom_local.value = new_value
-    
-    a.* = atom_local
-    atom_local
+    atomic_int { value: initial_value, increment_count: 0, decrement_count: 0, compare_swap_count: 0 }
 }
 
 func print_atomic_info(atomic_int a) {
--- kernel/locking/lock.s
+++ kernel/locking/lock.s
@@ -1,52 +1,14 @@
 package neurx.kernel.locking
 
-
-    mutex,
-    rwlock,
-    spinlock,
-    semaphore
-}
-
 struct lock {
-    lock_type lock_type
+    int lock_type
     int owner_id
     int wait_queue_size
-    bool is_locked
+    int is_locked
 }
 
-struct rwlock {
-    int reader_count
-    int writer_waiting
-    bool writer_active
-}
-
-func create_lock(lock_type lock_type) lock {
-    lock {
-        lock_type: lock_type,
-        owner_id: -1,
-        wait_queue_size: 0,
-        is_locked: false
-    }
-}
-
-func acquire_lock(lock* lock) (int, string) {
-    if lock.is_locked {
-        lock.wait_queue_size = lock.wait_queue_size + 1
-        (0, "Lock not acquired")
-    } else {
-        lock.is_locked = true
-        lock.owner_id = 0
-    0, ""
-    }
-}
-
-func release_lock(lock* lock) (int, string) {
-    lock.is_locked = false
-    lock.owner_id = -1
-    if lock.wait_queue_size > 0 {
-        lock.wait_queue_size = lock.wait_queue_size - 1
-    }
-    0, ""
+func create_lock(int lock_type) lock {
+    lock { lock_type: lock_type, owner_id: -1, wait_queue_size: 0, is_locked: 0 }
 }
 
 func try_acquire_lock(lock* lock) bool {
